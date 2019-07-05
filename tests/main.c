7668a73f2b91efbf09460ca685035cb98e74c090
Sarah Franc <sfranc@student.42.fr>
2019-07-05 11:49:07 +0200

    chore(Makefile): compil_tests

--- tests/main.c
+++ tests/main.c
@@ -1,7 +1,14 @@
-#include "libftasm.a"
+#include <stdio.h>
+#include <string.h>
+
+void	ft_bzero(void *s, size_t size);
 
 int		main(void)
 {
+	fprintf(stdout, "\nFT_BZERO\n");
+
+	char *s = "test bzero\n";
+	ft_bzero(s, strlen(s));
 
 	return (0);
 }
