00c6dd3c044acd1dc292eb6b208f19e2fc9d319b
Sarah Franc <sfranc@student.42.fr>
2019-07-05 11:33:58 +0200

    chore(clean): clean files

--- main.c
+++
@@ -1,7 +0,0 @@
-extern void		hello_world(void);
-
-int				main(void)
-{
-	hello_word();
-	return 0;
-}
--- srcs/ft_bzero.s
+++ srcs/ft_bzero.s
@@ -2,3 +2,12 @@ section .text
 	global _ft_bzero
 
 _ft_bzero:
+	mov rbp, rsp
+	sub rsp, 16
+
+	jmp leave
+
+leave:
+	mov rsp, rbp
+	pop rbp
+	ret
