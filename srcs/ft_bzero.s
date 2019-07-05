11c7c91830696ff5ef32a615a72bf59226417ffa
Sarah Franc <sfranc@student.42.fr>
2019-07-05 11:55:57 +0200

    feat(bzero): init

--- srcs/ft_bzero.s
+++ srcs/ft_bzero.s
@@ -1,9 +1,18 @@
 section .text
 	global _ft_bzero
 
+;void	ft_bzero(void *s, size_t size);
 _ft_bzero:
 	mov rbp, rsp
 	sub rsp, 16
+
+	.loop:
+		cmp rsi, 0
+		je leave
+		mov byte[rdi], 0
+		inc rdi
+		dec rsi
+		jmp .loop
 
 	jmp leave
 
