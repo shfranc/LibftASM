c00381f5020d842f6a8874f7a8c399d1410be8da
Sarah Franc <shfranc@gmail.com>
2019-07-21 20:20:36 +0200

    chore(test): clean main test

--- tests/cat_tests.sh
+++ tests/cat_tests.sh
@@ -3,7 +3,7 @@
 GREEN="\033[1;32m"
 RED="\033[1;31m"
 YELLOW="\033[1;33m"
-DEF="\033[0m"
+RESET="\033[0m"
 
 ft_cat="./tests/cat_tests"
 input_folder="./tests/input_files"
@@ -14,18 +14,18 @@ for file in "$input_folder"/*; do
 	ret="$(cat $file)"
 
 	if [[ "$ft_ret" == "$ret" ]]; then
-		printf "%-50s$GREEN%s$DEF\n" "$(basename $file)" "OK"
+		printf "%-50s$GREEN%s$RESET\n" "$(basename $file)" "OK"
 	else
-		printf "%-50s$RED%s$DEF\n" "$(basename $file)" "KO"
+		printf "%-50s$RED%s$RESET\n" "$(basename $file)" "KO"
 	fi
 
 done
 
 ft_ret=$($ft_cat "WRONG FD")
 	if [[ "$ft_ret" == "" ]]; then
-		printf "\n%-50s$GREEN%s$DEF\n" "Reading on WRONG FD" "OK"
+		printf "\n%-50s$GREEN%s$RESET\n" "Reading on WRONG FD" "OK"
 	else
-		printf "\n%-50s$RED%s$DEF\n" "Reading on WRONG FD" "KO"
+		printf "\n%-50s$RED%s$RESET\n" "Reading on WRONG FD" "KO"
 	fi
 
 printf "\nReading on STDIN:\n"
